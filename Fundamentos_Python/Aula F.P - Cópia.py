0c7837d0a037245452f8cd28f8e0ea0485565bb9
PauloVu1 <paulovu151@gmail.com>
2025-08-06 13:15:41 +0200

    duplicacoes

---
+++ Fundamentos_Python/Aula F.P - Cópia.py
@@ -0,0 +1,11 @@
+#Aula de 7/7/2025
+
+#tipos de dados primitivos em Python.   
+mansage= "hello"               #string    str   
+idade= 54                      #integer   int
+pi= 3.1415                     #float     float
+amazenamento_cheio = False     #bool      bool
+
+                                #type 
+# permite fazer o comentario de linha simples
+
